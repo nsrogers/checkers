4c896eebf30384617c66a66e72e301158f5f62f3
TJ White <thjwhite@gmail.com>
2014-02-18 15:42:06 -0800

    Started CPP stuffs

--- cpp_code.cc
+++ cpp_code.cc
@@ -1,3 +1,55 @@
+#include <vector>
+#define LEN 32
+using namespace std;
+
+class minMaxTree
+{
+    public:
+        int heuristic;
+        vector<vector<int> > moves;
+        vector<minMaxTree*> children;
+        
+        minMaxTree(vector<vector<int> >);
+};
+
+minMaxTree::minMaxTree(vector<vector<int> > mov)
+{
+    moves = mov;
+    heuristic = 0;
+}
+
+
+
+vector<vector<int> > possibleMoves(int* board)
+{
+    vector<vector<int> > moves;
+    for(int i = 0; i < LEN; i++)
+    {
+        if(board[i] > 0)
+        {
+            int offset = (i/4) % 2;
+            if(board[i + 4 - offset] == 0)
+            {
+                vector<int> move;
+                move.push_back(i);
+                move.push_back(i+4);
+                moves.push_back(move);
+            }
+            else if(board[i + 4 - offset] == 0)
+            {
+                vector<int> move;
+                move.push_back(i);
+                move.push_back(i+4);
+                moves.push_back(move);
+            }
+        }
+    }
+}
+
+
+
+
+
 /*
  * This function is called by the Python front end
  * Using ctypes
@@ -5,5 +57,6 @@
  */
 extern "C" int* callAI(int* board)
 {
+    
     return board;
 }
